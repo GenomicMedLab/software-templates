3a02761fecb51dc5d9b0e855a101812a1f8995db
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-07-11 09:09:03 -0400

    feat(python): define version

--- python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,1 +1,10 @@
 """{{ cookiecutter.description }}"""
+from importlib.metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version("cool_seq_tool")
+except PackageNotFoundError:
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
