9e04ad9af3ec35037d8c854d7a3c59971671263c
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-07-11 09:18:46 -0400

    ope

--- python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import PackageNotFoundError, version
 
 
 try:
-    __version__ = version("cool_seq_tool")
+    __version__ = version("{{ cookiecutter.project_slug }}")
 except PackageNotFoundError:
     __version__ = "unknown"
 finally:
