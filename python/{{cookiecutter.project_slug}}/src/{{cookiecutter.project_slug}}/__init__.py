e885621ef796aeff1fff5a441a233d7f792ce638
James Stevenson <james.stevenson@nationwidechildrens.org>
2024-07-12 08:30:43 -0400

    feat(python): define version (#45)

--- python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,1 +1,10 @@
 """{{ cookiecutter.description }}"""
+from importlib.metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version("{{ cookiecutter.project_slug }}")
+except PackageNotFoundError:
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
