6719ef79ff34e505d82b536e5d7f474494bd5b60
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-01-02 22:09:44 -0500

    duh

--- python/{{cookiecutter.project_slug}}/tests/test_api.py
+++ python/{{cookiecutter.project_slug}}/tests/test_api.py
@@ -18,11 +18,13 @@ def api_client():
 def test_service_info(api_client: TestClient):
     response = api_client.get("/service_info")
     assert response.status_code == 200
+    EXPECTED_VERSION_PATTERN = r"\d\.\d\."
     response_json = response.json()
     assert response_json["id"] == "org.genomicmedlab.{{ cookiecutter.project_slug }}"
     assert response_json["name"] == "{{ cookiecutter.project_slug }}"
     assert response_json["type"]["group"] == "org.genomicmedlab"
     assert response_json["type"]["artifact"] == "{{ cookiecutter.project_slug }} API"
+    assert re.match(EXPECTED_VERSION_PATTERN, response_json["type"]["version"])
     assert response_json["description"] == "{{ cookiecutter.description }}"
     assert response_json["organization"] == {
         "name": "Genomic Medicine Lab at Nationwide Children's Hospital",
@@ -35,13 +37,4 @@ def test_service_info(api_client: TestClient):
     )
     assert datetime.fromisoformat(response_json["createdAt"])
     assert ServiceEnvironment(response_json["environment"])
-
-
-def test_service_info_version(api_client: TestClient):
-    """Early in development, we expect `__version__` to be None, but it shouldn't be
-    once a version tag is made. Therefore, this test is broken out so that it can be easily
-    ignored while initial development is ongoing.
-    """
-    response_json = api_client.get("/service_info").json()
-    assert re.match(r"\d\.\d\.\d\.", response_json["type"]["version"])
-    assert re.match(r"\d\.\d\.\d\.", response_json["version"])
+    assert re.match(EXPECTED_VERSION_PATTERN, response_json["version"])
