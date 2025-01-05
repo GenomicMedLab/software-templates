beae971ce5598239fdfbc850434d45f345d22e92
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-01-04 20:11:50 -0500

    simplify

--- python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/logging.py
+++ python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/logging.py
@@ -8,9 +8,8 @@ def initialize_logs(log_level: int = logging.DEBUG) -> None:
 
     :param log_level: app log level to set
     """
-    log_filename = f"{__package__}.log"
     logging.basicConfig(
-        filename=log_filename,
+        filename=f"{__package__}.log",
         format="[%(asctime)s] - %(name)s - %(levelname)s : %(message)s",
     )
     logger = logging.getLogger(__package__)
