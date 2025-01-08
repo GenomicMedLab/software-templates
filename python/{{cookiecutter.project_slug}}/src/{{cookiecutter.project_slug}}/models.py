3cfc44c5b6f32596af5901c94e16e7f8718bac69
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-01-08 10:25:07 -0500

    feat: add fastapi + service_info endpoint (#78)
    * FastAPI api.py module, including a /service_info endpoint conforming to the GA4GH spec. I didn't bother implementing updatedAt (it's optional) because I think it'd be kind of a hassle to update a datetime object every time we make a release. Maybe there's a way to automate this but not a priority IMO.
    * An API tests module with reasonable coverage
    * A library config module. I don't think we have any services right now that quite map to the dev/test/staging/prod schema (I took this from rails), but some are pretty close and we could tweak as needed. The idea here is that it's a central source of truth about what kind of an environment the app is running in, and then other modules (like a database module or the main API views module) refer to it when making environment-based decisions. The individual settings here (test and debug) are sort of general/predictive of what we might need in a given app. Lots of examples online also set a DB URL or DB connection configs here as well. (I also looked at pydantic-settings for this but I think it's a lot more than we need. )
    * A logging setup module. This would also be shared with a CLI module (which is my next project for this repo).
    * Add FAST rules from ruff.

---
+++ python/{{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/models.py
@@ -0,0 +1,58 @@
+"""Define models for internal data and API responses."""
+
+from enum import Enum
+from typing import Literal
+
+from pydantic import BaseModel
+
+from . import __version__
+
+
+class ServiceEnvironment(str, Enum):
+    """Define current runtime environment."""
+
+    DEV = "dev"
+    PROD = "prod"
+    TEST = "test"
+    STAGING = "staging"
+
+
+class ServiceOrganization(BaseModel):
+    """Define service_info response for organization field"""
+
+    name: Literal["Genomic Medicine Lab at Nationwide Children's Hospital"] = (
+        "Genomic Medicine Lab at Nationwide Children's Hospital"
+    )
+    url: Literal[
+        "https://www.nationwidechildrens.org/specialties/institute-for-genomic-medicine/research-labs/wagner-lab"
+    ] = "https://www.nationwidechildrens.org/specialties/institute-for-genomic-medicine/research-labs/wagner-lab"
+
+
+class ServiceType(BaseModel):
+    """Define service_info response for type field"""
+
+    group: Literal["org.genomicmedlab"] = "org.genomicmedlab"
+    artifact: Literal["{{ cookiecutter.project_slug }} API"] = "{{ cookiecutter.project_slug }} API"
+    version: Literal[__version__] = __version__
+
+
+class ServiceInfo(BaseModel):
+    """Define response structure for GA4GH /service_info endpoint."""
+
+    id: Literal["org.genomicmedlab.{{ cookiecutter.project_slug }}"] = (
+        "org.genomicmedlab.{{ cookiecutter.project_slug }}"
+    )
+    name: Literal["{{ cookiecutter.project_slug }}"] = "{{ cookiecutter.project_slug }}"
+    type: ServiceType
+    description: Literal["{{ cookiecutter.description }}"] = "{{ cookiecutter.description }}"
+    organization: ServiceOrganization
+    contactUrl: Literal["Alex.Wagner@nationwidechildrens.org"] = (  # noqa: N815
+        "Alex.Wagner@nationwidechildrens.org"
+    )
+    documentationUrl: Literal["https://github.com/{{ cookiecutter.org }}/{{ cookiecutter.repo }}"] = (  # noqa: N815
+        "https://github.com/{{ cookiecutter.org }}/{{ cookiecutter.repo }}"
+    )
+    createdAt: Literal["{% now 'utc', '%Y-%m-%dT%H:%M:%S+00:00' %}"] = "{% now 'utc', '%Y-%m-%dT%H:%M:%S+00:00' %}"  # noqa: N815
+    updatedAt: str | None = None  # noqa: N815
+    environment: ServiceEnvironment
+    version: Literal[__version__] = __version__
